c78a15ae9373beaa5fc784d9db148e49894a8f22
Andrey <gbou883@gmail.com>
2024-12-21 13:21:02 +0300

    chore: adjusted reward quantities

--- src/utils/helpers/calculateReward.ts
+++ src/utils/helpers/calculateReward.ts
@@ -16,9 +16,9 @@ export function calculateReward(
     switch (format) {
       case "1x1":
         if (players >= 4 && players <= 7) {
-          return [30, 20, 10];
+          return [40, 30, 20];
         } else if (players >= 8 && players <= 13) {
-          return [50, 30, 20];
+          return [60, 40, 30];
         } else if (players >= 14 && players <= 17) {
           return [70, 50, 30];
         } else if (players >= 18) {
@@ -27,9 +27,9 @@ export function calculateReward(
         break;
       case "2x2":
         if (players >= 4 && players <= 7) {
-          return [30, 20, 10];
+          return [40, 30, 20];
         } else if (players >= 8 && players <= 12) {
-          return [50, 30, 20];
+          return [60, 40, 30];
         } else if (players >= 13) {
           return [80, 60, 40];
         }
@@ -37,7 +37,7 @@ export function calculateReward(
       case "3x3":
       case "5x5":
         if (players >= 4 && players <= 6) {
-          return [30, 20, 10];
+          return [40, 30, 20];
         } else if (players >= 7) {
           return [80, 60, 40];
         }
