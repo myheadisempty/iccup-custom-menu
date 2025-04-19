3306fed1a647a42d619b9d6005f9d65a648557ac
Andrey <gbou883@gmail.com>
2025-04-19 13:43:06 +0300

    migrate to Tailwind v4, add tooltip to copy button, update dependencies

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Tooltip } from "antd";
 import { FC, useEffect, useState } from "react";
 import IconContentCopy from "./icons/IconContentCopy";
 import IconCheck from "./icons/IconCheck";
@@ -25,27 +25,35 @@ export const CopyButton: FC<CopyButtonProps> = ({ reportText }) => {
     setCopied(true);
   };
 
+  const tooltipTitle = !reportText.trim()
+    ? null
+    : copied
+    ? "Скопировано"
+    : "Скопировать";
+
   return (
-    <Button
-      type="text"
-      onClick={handleClick}
-      className="text-gray-400 rounded ml-auto hover:bg-zinc-900 hover:text-gray-400 relative disabled:opacity-[0.65] disabled:hover:bg-transparent"
-      disabled={reportText.trim() === ""}
-    >
-      <span
-        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${
-          copied ? "scale-0 opacity-0" : "scale-100 opacity-100"
-        }`}
+    <Tooltip title={tooltipTitle}>
+      <Button
+        type="text"
+        onClick={handleClick}
+        className="text-gray-400 rounded-sm ml-auto hover:bg-zinc-900 hover:text-gray-400 relative disabled:opacity-[0.65] disabled:hover:bg-transparent"
+        disabled={!reportText.trim()}
       >
-        <IconContentCopy />
-      </span>
-      <span
-        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${
-          copied ? "scale-100 opacity-100" : "scale-0 opacity-0"
-        }`}
-      >
-        <IconCheck />
-      </span>
-    </Button>
+        <span
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${
+            copied ? "scale-0 opacity-0" : "scale-100 opacity-100"
+          }`}
+        >
+          <IconContentCopy />
+        </span>
+        <span
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${
+            copied ? "scale-100 opacity-100" : "scale-0 opacity-0"
+          }`}
+        >
+          <IconCheck />
+        </span>
+      </Button>
+    </Tooltip>
   );
 };
